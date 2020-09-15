cc52ae89150678d462c4f99d7c1f4f78b664cc85
Vanshika Shukla <vanshika.shukla@indiamart.com>
2020-09-15 22:10:26 +0530

    Initial Commit

---
+++ CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Vanshika Shukla on 15/09/20.
+//  Copyright © 2020 xyz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
